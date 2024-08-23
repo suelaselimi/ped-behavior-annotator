5fad9da4d8c8c15c85f2cb0e00fb3ea7a36fa070
suelaselimi <suelaselimi07@gmail.com>
2024-08-23 15:17:44 +0200

    msms

---
+++ anything.py
@@ -0,0 +1,9 @@
+import TKinterModernThemes as TKMT
+from functools import partial
+import tkinter as tk
+import json
+class App(TKMT.ThemedTKinterFrame):
+    def __init__(self):
+        super().__init__("TKinter Custom Themes Demo")
+        
+App()
